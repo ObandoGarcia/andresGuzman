a6b41f8a316eb40fc507378234fc16a465ef3e6f
Jose Manuel Obando <57765554+ObandoGarcia@users.noreply.github.com>
2024-12-26 22:08:11 -0600

    Eliminacion de la rama de microservices

--- microservicios/springcloud.msvc/src/main/java/com/obando/springcloud/msvc/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.obando.springcloud.msvc.repositories;
-
-import com.obando.springcloud.msvc.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
