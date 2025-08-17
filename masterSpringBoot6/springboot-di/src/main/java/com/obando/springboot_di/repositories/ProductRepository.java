491b1127c8944b534a5860e1095f0cec4ead3f3e
Jose Manuel Obando <57765554+ObandoGarcia@users.noreply.github.com>
2025-08-16 22:58:07 -0600

    Delete masterSpringBoot6 directory

--- masterSpringBoot6/springboot-di/src/main/java/com/obando/springboot_di/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.obando.springboot_di.repositories;
-
-import com.obando.springboot_di.models.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
--- masterSpringBoot6/springboot-di/src/main/java/com/obando/springboot_di/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.obando.springboot_di.repositories;
-
-import com.obando.springboot_di.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
