105d19753de48683abe28d8fa5742211a0c38c7e
Jose Manuel Obando <57765554+ObandoGarcia@users.noreply.github.com>
2025-08-16 22:58:18 -0600

    Delete masterJava directory

--- masterJava/spring/spring-backend/src/main/java/com/obando/curso/backend/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.obando.curso.backend.repositories;
-
-import com.obando.curso.backend.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
